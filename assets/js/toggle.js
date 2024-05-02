26ed92c48ce6f8b56afc15697d4cf2b99f8155a8
jean-philippe.bazonnais <jean-philippe.bazonnais@ign.fr>
2024-05-02 17:07:46 +0200

    fix switch sur les themes

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -7,8 +7,8 @@
 })();
 
 function toggleTheme () {
-    var portail = document.getElementById("portail");
-    var dsfr = document.getElementById("dsfr");
+    var portail = document.querySelectorAll("link[id='portail']");
+    var dsfr = document.querySelectorAll("link[id^=dsfr]");
 
     if (localStorage.getItem("theme") === "portail") {
         localStorage.setItem("theme", "dsfr");
@@ -23,10 +23,14 @@ function toggleTheme () {
     }
 }
 
-function enableStylesheet (node) {
-    node.rel = "stylesheet";
+function enableStylesheet (nodes) {
+    nodes.forEach(node => {
+        node.rel = "stylesheet";
+    });
 }
 
-function disableStylesheet (node) {
-    node.rel = "alternate stylesheet";
+function disableStylesheet (nodes) {
+    nodes.forEach(node => {
+        node.rel = "alternate stylesheet";
+    });
 }
