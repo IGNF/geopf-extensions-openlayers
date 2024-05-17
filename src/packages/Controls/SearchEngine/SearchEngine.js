25439c626c9452d1412ca0d1a3ae3c3bcb276e3d
jean-philippe.bazonnais <jean-philippe.bazonnais@ign.fr>
2024-05-17 18:26:25 +0200

    Option active par defaut pour l'ajout d'une couche

--- src/packages/Controls/SearchEngine/SearchEngine.js
+++ src/packages/Controls/SearchEngine/SearchEngine.js
@@ -71,7 +71,7 @@ var logger = Logger.getLogger("searchengine");
  * @param {Array}   [options.resources.autocomplete] - resources autocompletion, by default : ["PositionOfInterest", "StreetAddress"]
  * @param {Boolean} [options.resources.search = false] - false to disable search service, by default : "false"
  * @param {Object}  [options.searchOptions = {}] - options of search service
- * @param {Boolean} [options.searchOptions.addToMap = false] - add layer automatically to map, defaults to false.
+ * @param {Boolean} [options.searchOptions.addToMap = true] - add layer automatically to map, defaults to true.
  * @param {String}  [options.searchOptions.filterServices] - filter on a list of search services, each field is separated by a comma. "WMTS,TMS" by default
  * @param {String}  [options.searchOptions.filterVectortiles] - filter on list of search layers only on service TMS, each field is separated by a comma. "PLAN.IGN, ..." by default
  * @param {Boolean} [options.searchOptions.updateVectortiles = false] - updating the list of search layers only on service TMS
@@ -281,7 +281,7 @@ var SearchEngine = class SearchEngine extends Control {
             advancedSearch : {},
             coordinateSearch : {},
             searchOptions : {
-                addToMap : false,
+                addToMap : true,
                 serviceOptions : {}
             },
             geocodeOptions : {
