1381b6b1e2d180fe02c9d32a3fe435029c293d3f
jean-philippe.bazonnais <jean-philippe.bazonnais@ign.fr>
2025-07-25 17:48:32 +0200

    icon avec nom en dsfr : fr-icon-*

--- src/packages/Controls/LayerSwitcher/LayerSwitcherDOM.js
+++ src/packages/Controls/LayerSwitcher/LayerSwitcherDOM.js
@@ -959,9 +959,16 @@ var LayerSwitcherDOM = {
             Object.assign(button.style, opts.styles);
 
             if (opts.icon) {
-                // FIXME width / height à definir si ces options ne sont pas renseignées inline
                 if (opts.icon.startsWith("<svg")) {
+                    // FIXME 
+                    // width / height à definir si ces options ne sont pas renseignées inline
                     opts.icon = "data:image/svg+xml;base64," + btoa(opts.icon);
+                } else if (opts.icon.startsWith("fr-icon")) {
+                    // FIXME
+                    // à optimiser, le texte est masqué (label) !?
+                    button.classList.add(opts.icon);
+                } else {
+                    // url...
                 }
             } else {
                 var iconDefault = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M23 12L15.9289 19.0711L14.5147 17.6569L20.1716 12L14.5147 6.34317L15.9289 4.92896L23 12ZM3.82843 12L9.48528 17.6569L8.07107 19.0711L1 12L8.07107 4.92896L9.48528 6.34317L3.82843 12Z"></path></svg>`;
