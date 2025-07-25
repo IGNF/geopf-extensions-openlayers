c5805fb4f76436f89f04db5abe0d98f488aa5390
jean-philippe.bazonnais <jean-philippe.bazonnais@ign.fr>
2025-07-25 13:58:50 +0200

    icone par defaut

--- src/packages/Controls/LayerSwitcher/LayerSwitcherDOM.js
+++ src/packages/Controls/LayerSwitcher/LayerSwitcherDOM.js
@@ -963,6 +963,9 @@ var LayerSwitcherDOM = {
                 if (opts.icon.startsWith("<svg")) {
                     opts.icon = "data:image/svg+xml;base64," + btoa(opts.icon);
                 }
+            } else {
+                var iconDefault = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M23 12L15.9289 19.0711L14.5147 17.6569L20.1716 12L14.5147 6.34317L15.9289 4.92896L23 12ZM3.82843 12L9.48528 17.6569L8.07107 19.0711L1 12L8.07107 4.92896L9.48528 6.34317L3.82843 12Z"></path></svg>`;
+                opts.icon = "data:image/svg+xml;base64," + btoa(iconDefault);
             }
 
             if (!document.querySelector(`style[data-injected="${className.toLowerCase()}"]`)) {
