eceb8154b1678c29c18f7f23f0f26c769ec4ae46
MatRouillard <mathis.rouillard@ign.fr>
2026-05-11 12:02:38 +0200

    fix(toggle): Modif positon des toggle pour adapter aux position-container

--- src/packages/Controls/Draw/Draw.js
+++ src/packages/Controls/Draw/Draw.js
@@ -27,7 +27,7 @@ import { SelectEvent } from "ol/interaction/Select";
  * @property {String|HTMLElement} [icon="fr-icon-pen-nib-line"] - Classe à ajouter au bouton ou élément svg (inline) ou élément HTML à ajouter avant le label (type span).
  * @property {String} [title="Annoter la carte"] Attribut title / aria-label.
  * @property {String} [size="sm"] Taille du panneau ("sm" ou "lg").
- * @property {String} [position=right] Position du panneau ("left" ou "right").
+ * @property {String} [position] - Position CSS du widget sur la carte. Valeurs acceptées : `top-right`, `top-left`, `bottom-right` ou `bottom-left`. Si non donné, le contrôle doit être positionné en CSS.
  * @property {Select} [select] Interaction de sélection lié au contrôle. Si aucune interaction n'est donnée, ajoute une interaction de type {@link SelectingInteraction SelectingInteraction}.
  * @property {VectorSource} [source] Source à ajouter au contrôle initialement. Peut-être fait après via la méthode `setSource`. Si aucune source n'est donnée, en ajoute une de base.
  * @property {Boolean|Modify} [modify=true] Si faux, n'ajoute pas d'interaction pour modifier les objets. Sinon, ajoute une interaction de type {@link ModifyingInteraction ModifyingInteraction}, héritant de {@link https://openlayers.org/en/latest/apidoc/module-ol_interaction_Modify-Modify.html Modify}, qui s'active à la sélection d'un objet. Une interaction de type `Modify` peut aussi être passée en paramètre (auquel cas ).
